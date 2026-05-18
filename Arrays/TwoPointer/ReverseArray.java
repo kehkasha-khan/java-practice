6507fe1c9108f70cd6036c75472c7a8fa518a389
kehkasha-khan <76685552+kehkasha-khan@users.noreply.github.com>
2026-05-19 00:39:18 +0530

    Create ReverseArray.java

---
+++ Arrays/TwoPointer/ReverseArray.java
@@ -0,0 +1,14 @@
+Two Pointer Approach is a technique where two indexes are used to solve problems efficiently instead of using nested loops.
+  🎯Why do we use it?
+    Because it:
+    ✅ reduces time complexity
+    ✅ makes code faster
+    ✅ avoids nested loops
+  
+  🎯 Use Two Pointer When…
+      You need to:
+      ✅ compare elements
+      ✅ traverse from both sides
+      ✅ reduce nested loops
+      ✅ process pairs efficiently
+  
