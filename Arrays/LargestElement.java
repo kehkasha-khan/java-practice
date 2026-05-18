8d5d75c795837edf6ae73e053438a52424df62e7
kehkasha-khan <76685552+kehkasha-khan@users.noreply.github.com>
2026-05-19 00:15:57 +0530

    Update LargestElement.java

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,1 +1,17 @@
+finding largest element of array
+  class LargestElement {
+    public static void main(String[] args) {
+       int[]  numbers = {1, 34, 56, 23, 76, 87};
+       int max = numbers[0];
+       System.out.println("Printing larget element of array");
+       for(int i=0; i < numbers.length; i++){
+        if(numbers[i] > max)      
+        max = numbers[i];
+       }
+       System.out.print(max);
+    }
+}
 
+output:
+Printing larget element of array
+87
