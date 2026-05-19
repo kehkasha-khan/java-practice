e44fe5821ed1f3df15b25f37e900e8c65da74010
kehkasha-khan <76685552+kehkasha-khan@users.noreply.github.com>
2026-05-19 22:41:52 +0530

    Update SmallestElement.java

--- Arrays/Basics/SmallestElement.java
+++ Arrays/Basics/SmallestElement.java
@@ -1,1 +1,12 @@
-
+find the smallest element of array
+  public class SmallestElement{
+  public static void main(String [] args){
+    int[] numbers = {5, 2, 9, 1, 7};
+    int min = numbers[0];
+    for(int i=0; i<numbers.length; i++){
+      if(numbers[i] < min){
+        min = numbers[i];
+      }}
+      System.out.println(min);
+    }
+}
