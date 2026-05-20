696726e6353a7e4a2fcd0787aed98bc9eee170b2
kehkasha-khan <76685552+kehkasha-khan@users.noreply.github.com>
2026-05-20 15:58:44 +0530

    Update PrintCharacter.java

--- Strings/PrintCharacter.java
+++ Strings/PrintCharacter.java
@@ -1,1 +1,23 @@
 
+
+class PrintCharacter {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        String word = "Kehkasha ";
+        for(int i = 0; i < word.length(); i++)
+        {
+            System.out.println( word.charAt(i));
+            
+        }
+    }
+}
+
+output:
+K
+e
+h
+k
+a
+s
+h
+a
